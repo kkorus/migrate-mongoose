9cf6400831ed7f8d53e3d5fb018690229aa5f923
Konrad Korus <konrad.korus@flipfit.com>
2022-05-23 14:45:50 +0200

    bump mongoose version

--- examples/config-file-usage/db/models/index.js
+++ examples/config-file-usage/db/models/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const User = require('./user.model');
 
-mongoose.connect('mongodb://localhost:27017/mydb', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/mydb', {});
 
 module.exports = {
-    User
+  User,
 };
